42bdbd44307435a11a76ee9d66cada81f4fedfdf
cestevespr <43469157+cestevespr@users.noreply.github.com>
2022-10-01 12:33:22 -0400

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Chris Esteves
+
+# *I am interested in data science as it provides an opportunity to gain better insights into the world around us.
+
+# ### Running code to display inputs into a sentence
+
+# In[2]:
+
+
+first_name = "Chris"
+speaking_language = "Spanish"
+
+print(f"Hi, my name is {first_name} and I speak {speaking_language}.")
+
+
+# - three
+# - two
+# - one
+# 
+# | **Abra** | **Kadabra** | 
+# | --- | ----------- |
+# | *Presto* | *Change-o* |
+# | 1 | 2 |
